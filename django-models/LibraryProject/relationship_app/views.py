89b43e61962d40838c54dffe6f5bd9e7b586032d
Mechanic <dumashieanthony@gmail.com>
2025-11-09 20:05:48 +0000

    corrected views code

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,16 +5,16 @@ from .models import Book, Library
 # Function-Based View: List all books
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    # Use the full path relative to the templates folder
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 
 # Class-Based View: Display details of a specific library
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
-    # Optional: override get_object to fetch by library name from URL
     def get_object(self):
         library_name = self.kwargs.get('library_name')
         return get_object_or_404(Library, name=library_name)
