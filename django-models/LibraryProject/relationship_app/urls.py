f08d0daacc4ccdaaa07b5d0813262ecbcda9579a
Mechanic <dumashieanthony@gmail.com>
2025-11-09 20:22:36 +0000

    urls fix

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,27 +1,10 @@
-"""
-URL configuration for LibraryProject project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
-from . import views
+from .views import list_books, LibraryDetailView  # Explicit import as required by ALX
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('books/', views.list_books, name='list_books'),
+    # Function-based view
+    path('books/', list_books, name='list_books'),
 
-    # Class-based view URL
-    path('library/<str:library_name>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    # Class-based view
+    path('library/<str:library_name>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
